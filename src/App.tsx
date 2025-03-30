fed491eb51a6df31c426d7705350433addf64751
scanxpertweb <scanxpert1@gmail.com>
2025-03-30 10:48:58 +0530

    toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import Footer from "./components/Footer/footer";
 import Navbar from "./components/navbar/Navbar";
 import AppRouter from "./routes/Router";
-
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={8}
+        toastOptions={{
+          duration: 5000,
+          style: {
+            background: "#363636",
+            color: "#fff",
+          },
+          success: {
+            duration: 3000,
+            style: {
+              background: "green",
+              color: "#fff",
+            },
+          },
+          error: {
+            duration: 3000,
+            style: {
+              background: "#ff4b4b",
+              color: "#fff",
+            },
+          },
+        }}
+      />
       <Navbar />
       <div className="mt-4">
         <AppRouter />
